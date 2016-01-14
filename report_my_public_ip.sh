2849a1393b4b0418d64387e53b8ef3ef0f173d2e
George.Ef <arkroan@gmail.com>
2016-01-14 13:58:09 +0200

    Fixed IP reporting file

--- report_my_public_ip.sh
+++ report_my_public_ip.sh
@@ -3,13 +3,14 @@
 # Checks for my public ip from the ipify api
 # and stores it in : /home/user/MEGAsync/xbmc-htpc_home_public_ip.txt
 syncfolder='/home/user/MegaSyncFolder'
+sfile='xbmc_htpc_public_ip.txt'
 
-curl 'https://api.ipify.org?format=json' > /home/user/MEGAsync/xbmc-htpc_home_public_ip.txt
-echo " " >>  $syncfolder/xbmc-htpc_home_public_ip.txt
+curl 'https://api.ipify.org' > $syncfolder/$sfile
+echo " " >>  $syncfolder/$sfile
 
 # report ports
-echo "Live Webcam:		:8090" >>  $syncfolder/xbmc-htpc_ports.txt
-echo "Recordings :              :8090" >>  $syncfolder/xbmc-htpc_ports.txt
+echo "Live Webcam:		:8090" >>  $syncfolder/$sfile
+echo "Recordings :              :8090" >>  $syncfolder/$sfile
 
 exit 0
 
